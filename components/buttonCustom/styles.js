be52cc1f5423e57fec0d946cbd3f629e7f7167cd
Marco Fiorito <marco@xmartlabs.com>
2019-03-18 09:10:57 -0300

    Button functionality

---
+++ components/buttonCustom/styles.js
@@ -0,0 +1,14 @@
+import { Colors } from "../../colors/Colors";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: "center"
+  },
+  buttonText: {
+    color: Colors.pink,
+    fontSize: 14,
+    lineHeight: 32,
+    fontFamily: "SourceSansPro-SemiBold"
+  }
+});
